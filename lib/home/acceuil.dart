36dfe295075643302dd06fc4d4756d5937a6ca43
AGRIGNAN Abdou Manaf <agrignanmanaf6@gmail.com>
2025-10-14 12:34:18 +0000

    modification d'accueil

--- lib/home/acceuil.dart
+++ lib/home/acceuil.dart
@@ -12,7 +12,21 @@ class _AccueilScreenState extends State<AccueilScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
-        title: Text("Home"),
+        backgroundColor: const Color.fromARGB(255, 7, 100, 175),
+        title: Text("Focus Pro",style: TextStyle(
+          color: Colors.white
+        ),),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              width: 70,
+              height: 70,
+              child: Image.asset("/assets/images/image.png"),
+            )
+          ],
+        ),
       ),
     );
   }
